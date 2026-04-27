a7b87b8ee7aaed51603471791b072377323d00f4
bohanz <bohan_z@mit.edu>
2026-04-27 13:35:03 +0000

    chore: chain merge in lora_merge.sh

--- lora_merge.sh
+++ lora_merge.sh
@@ -2,3 +2,13 @@ lora_add ./output_2000_4/mygo_sdxl_pti/lora_weight.safetensors \
   ./output_2000_4/jojo_sdxl_pti/lora_weight.safetensors \
   ./output_2000_4/mygo_jojo_merged.safetensors \
   --mode ljl-sdxl
+
+lora_add ./output_2000_4/wushan_sdxl_pti/lora_weight.safetensors \
+  ./output_2000_4/mygo_jojo_merged.safetensors \
+  ./output_2000_4/mygo_jojo_wushan_merged.safetensors \
+  --mode ljl-sdxl
+
+lora_add ./output_2000_4/cyberpunk_all_sdxl_pti/lora_weight.safetensors \
+  ./output_2000_4/mygo_jojo_wushan_merged.safetensors \
+  ./output_2000_4/four_merged_all.safetensors \
+  --mode ljl-sdxl
